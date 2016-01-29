56cb85533ee05723e3b2daa34f350aaba0159fb8
Drew Potter <hello@drewpotter.com>
2016-01-29 14:17:57 +0000

    Add variable playground area.

---
+++ variable-playground.js
@@ -0,0 +1,31 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Andrew',
+// 	// 	age: 24
+// 	// };
+
+// 	obj.age = 24;
+
+// };
+
+// updatePerson(person);
+// console.log(person);
+
+// Array example
+
+var grades = [15, 88]
+
+function addGrades (gradesArray) {
+	gradesArray.push(55);
+	debugger;
+	//gradesArray = [12, 33, 99];
+};
+
+addGrades(grades);
+console.log(grades);
+
